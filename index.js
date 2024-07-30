fca365854e259892f2c45f8ed8a4123759a2d5e4
PhoenixTech2003 <phoenixtech2003@gmail.com>
2024-07-30 15:51:58 +0200

    Refactor to express

--- index.js
+++ index.js
@@ -4,28 +4,56 @@ const url = require("node:url");
 const express = require("express");
 
 const app = express();
+const PORT = 9000
 
-http
-  .createServer(function (req, res) {
-    const q = url.parse(req.url, true);
-    const paths = {
-      "/": "index.html",
-      "/about": "about.html",
-      "/contact-me": "contact-me.html",
-    };
-
-    if (q.pathname in paths) {
-      res.writeHead(200, { "Content-Type": "Text/html" });
-      fs.readFile(paths[q.pathname], function (err, data) {
-        res.write(data);
-        res.end();
-      });
-    } else {
-      res.writeHead(404, { Error: "Resource not found" });
-      fs.readFile("./404.html", function (err, data) {
-        res.write(data);
-        res.end();
-      });
-    }
+try{
+  app.get('/',(req,res)=>{
+    fs.readFile('./index.html',function(err,data){
+      res.write(data)
+      res.end()
+    })
   })
-  .listen(8080);
+
+  app.get('/about',(req,res)=>{
+    fs.readFile('./about.html',function(err,data){
+      res.write(data)
+      res.end()
+    })
+  })
+
+  app.get('/contact-me',(req,res)=>{
+    fs.readFile('./contact-me.html',function(err,data){
+      res.write(data)
+      res.end()
+    })
+  })
+
+  app.listen(PORT,()=> console.log("express server has launched"))
+}catch(err){
+  console.log("404 error found")
+}
+
+// http
+//   .createServer(function (req, res) {
+//     const q = url.parse(req.url, true);
+//     const paths = {
+//       "/": "index.html",
+//       "/about": "about.html",
+//       "/contact-me": "contact-me.html",
+//     };
+
+//     if (q.pathname in paths) {
+//       res.writeHead(200, { "Content-Type": "Text/html" });
+//       fs.readFile(paths[q.pathname], function (err, data) {
+//         res.write(data);
+//         res.end();
+//       });
+//     } else {
+//       res.writeHead(404, { Error: "Resource not found" });
+//       fs.readFile("./404.html", function (err, data) {
+//         res.write(data);
+//         res.end();
+//       });
+//     }
+//   })
+//   .listen(8080);
