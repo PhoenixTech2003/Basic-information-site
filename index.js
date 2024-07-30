177e34acaf601ff1817278afd9b50b57caa12cc2
PhoenixTech2003 <phoenixtech2003@gmail.com>
2024-07-30 10:35:07 +0200

    Implement routing using node js

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require("node:http");
+const fs = require("node:fs");
+const url = require("node:url");
+
+http
+  .createServer(function (req, res) {
+    const q = url.parse(req.url, true);
+    const paths = {
+      "/": "index.html",
+      "/about": "about.html",
+      "/contact-me": "contact-me.html",
+    };
+
+    if (q.pathname in paths) {
+      res.writeHead(200, { "Content-Type": "Text/html" });
+      fs.readFile(paths[q.pathname], function (err, data) {
+        res.write(data);
+        res.end();
+      });
+    } else {
+      res.writeHead(404, { Error: "Resource not found" });
+      fs.readFile("./404.html", function (err, data) {
+        res.write(data);
+        res.end();
+      });
+    }
+  })
+  .listen(8080);
